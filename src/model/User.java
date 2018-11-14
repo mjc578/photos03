7a69df2042e83de799190c43ddeba5273bc02979
Michael Chapman <mjc578@gmail.com>
2018-11-13 21:50:24 -0500

    user class

--- src/model/User.java
+++ src/model/User.java
@@ -1,7 +1,23 @@
 package model;
 
+import java.util.*;
+
 public class User {
 	
 	private String username;
-
+	private List<AlbumInfo> userAlbums;
+	
+	public User(String username) {
+		this.username = username;
+		userAlbums = new ArrayList<AlbumInfo>();
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	//adds to the user's list of albums
+	public void addToAlbums(AlbumInfo album) {
+		userAlbums.add(album);
+	}
 }
